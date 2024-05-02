5978a8cbd1c1c14f4ef12a1441d67fc0627fe11e
Anton Skalaban <antonskalaban99@gmail.com>
2024-05-02 22:39:04 +0300

    feat: add month tests

---
+++ src/tests/month.test.ts
@@ -0,0 +1,17 @@
+import { getDaysInMonth, getMonthName, getNextMonth, getPrevMonth } from "utils/index";
+import { date } from "./mocks";
+
+describe("Month tests", () => {
+  it("should get month name", () => {
+    expect(getMonthName(date)).toStrictEqual("December");
+  });
+  it("should get days in month", () => {
+    expect(getDaysInMonth(date)).toStrictEqual(31);
+  });
+  it("should get prev month", () => {
+    expect(getPrevMonth(date.getFullYear(), date.getMonth())).toStrictEqual([1999, 10]);
+  });
+  it("should get next month", () => {
+    expect(getNextMonth(date.getFullYear(), date.getMonth())).toStrictEqual([2000, 0]);
+  });
+});
